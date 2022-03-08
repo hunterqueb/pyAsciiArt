82bbe3dadef4adfdef15a8e38d684c1010d3416c
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2022-03-08 12:41:03 -0500

    added comments

--- main.py
+++ main.py
@@ -15,39 +15,43 @@ def translate(value, inputMax, outputMax):
 ascii = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
 
 if __name__ == "__main__":
+    # get args
     imageName = sys.argv[1]
     if len(sys.argv) > 2:
         basewidth = int(sys.argv[2])
     else:
         basewidth = 300
+    
+    f = open("image.txt", "w")
+
+    # image processing:
+    # open image
     img = Image.open(imageName)
+    # calculated new height based on new width 
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     # resize image and save
     img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    # convert image to numpy array, this is of the form [:,:,3]
     img = np.array(img)
+    # convert 3d array to 2d array in gray scale
     gray = rgb2gray(img)    
 
-    f = open("image.txt", "w")
+    NUM_ASCII_CHARS = len(ascii)
 
-    numAsciiChars = len(ascii)
+    grayConverted = np.zeros_like(gray).astype(int)
 
-    # 0 > 255
-    # 0 > numAsciiChars
-
-    grayConverted = np.zeros_like(gray)
-
-    # translate the gray scale array to the translate gray scale so we can access the correct ascii character
+    # translate the gray scale array to the translate gray scale so we can access the correct ascii character and then writes to file in same loop
     for i in range(len(gray[:,1])):
-        for j in range(len(gray[1,:])):
-            grayConverted[i,j]= translate(gray[i,j],255,numAsciiChars)
-
-    grayConverted = grayConverted.astype(int)
-
-    for i in range(len(gray[:,1])):
+        # for every new line write to file new line character
         f.write("\n")
         for j in range(len(gray[1,:])):
-            string = ascii[grayConverted[i,j]]
-            f.write(string)
+            # for every pixel in a horizontal line:
+            # convert the gray scale pixel to an integer corresponding to the total ascii string
+            grayConverted = translate(gray[i,j],255,NUM_ASCII_CHARS)
+
+            string = ascii[grayConverted]
+            # then write the sting to the file
+            f.write(string + ' ')
 
     f.close()
